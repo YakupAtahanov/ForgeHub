6582b984230fd47f86fbfa74cc0ec9b57b165668
YakupAtahanov <yakup.atahanow.b@gmail.com>
2026-04-23 16:41:22 -0700

    Resetting project

--- apps/web/src/api/defaults.js
+++
@@ -1,4 +0,0 @@
-/** Seeded sample project on the API (see apps/api/src/sample-data.ts). */
-export const DEFAULT_API_PROJECT_ID = 'proj_sample_computer'
-export const DEFAULT_API_BASE_SNAPSHOT_ID = 'snap_001'
-export const DEFAULT_API_TARGET_SNAPSHOT_ID = 'snap_002'
